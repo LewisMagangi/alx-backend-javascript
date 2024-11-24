1865df5c502023838220e82b96a3a30360df98c2
LewisMagangi <lewismomanyi34@gmail.com>
2024-11-24 19:55:07 +0000

    eslint and jest fixes

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
 export default function returnHowManyArguments(...args) {
-    return args.length; // Return the number of arguments passed
+  return args.length; // Return the number of arguments passed
 }

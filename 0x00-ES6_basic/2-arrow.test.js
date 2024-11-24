6b201321334e8c1722235be4b45164fe92d4b6a2
LewisMagangi <lewismomanyi34@gmail.com>
2024-11-24 16:30:08 +0000

    Eslint on the arrow function.

---
+++ 0x00-ES6_basic/2-arrow.test.js
@@ -0,0 +1,7 @@
+import getNeighborhoodsList from './2-arrow.js';
+
+test('returns the list of neighborhoods', () => {
+  const neighborhoodsList = new getNeighborhoodsList();
+  expect(neighborhoodsList.addNeighborhood('Tenderloin')).toEqual(['SOMA', 'Union Square', 'Tenderloin']);
+});
+

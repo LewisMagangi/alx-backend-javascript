c25593b3cb304ef18a487ac72267ab449b263893
LewisMagangi <lewismomanyi34@gmail.com>
2024-11-27 01:27:02 -0800

    Eslint fix for some files and updating other files

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,44 +1,22 @@
 const fs = require('fs');
-const path = require('path');
 
-function countStudents(filePath) {
+function countStudents(path) {
   try {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    const rows = data.split('\n').filter(row => row.trim() !== '');
-
-    if (rows.length <= 1) {
-      throw new Error('Cannot load the database');
-    }
-
-    const headers = rows[0].split(',');
-    const studentCounts = {};
-
-    rows.slice(1).forEach((row) => {
-      const fields = row.split(',');
-
-      if (fields.length !== headers.length) {
-        return;
-      }
-
-      headers.forEach((header, index) => {
-        if (!studentCounts[header]) {
-          studentCounts[header] = [];
-        }
-        studentCounts[header].push(fields[index]);
-      });
+    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const obj = {};
+    data.forEach((el) => {
+      const student = el.split(',');
+      if (!obj[student[3]]) obj[student[3]] = [];
+      obj[student[3]].push(student[0]);
     });
-
-    const totalStudents = rows.slice(1).length;
-
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const field in studentCounts) {
-      console.log(`Number of students in ${field}: ${studentCounts[field].length}. List: ${studentCounts[field].join(', ')}`);
+    for (const cls in obj) {
+      if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
     }
   } catch (err) {
-    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
 module.exports = countStudents;
-
